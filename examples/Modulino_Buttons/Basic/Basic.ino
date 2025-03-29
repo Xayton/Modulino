1c6604cb7ea011e4266298a7dd653e593f490d0a
Xayton <30591904+Xayton@users.noreply.github.com>
2025-03-29 11:03:47 +0100

    Buttons turn on and off the corresponding led

--- examples/Modulino_Buttons/Basic/Basic.ino
+++ examples/Modulino_Buttons/Basic/Basic.ino
@@ -6,31 +6,37 @@
  * SPDX-License-Identifier: MPL-2.0
  */
 
-#include <Modulino.h>
+ #include <Modulino.h>
 
-ModulinoButtons buttons;
-
-bool button_a = false;
-bool button_b = false;
-bool button_c = false;
-
-void setup() {
-  Serial.begin(9600);
-  Modulino.begin();
-  buttons.begin();
-  //function to control the LEDs on top of each button
-  buttons.setLeds(true, true, true);
-}
-void loop() {
-  //request new data from the Modulino buttons
-  if (buttons.update()) {
-    //Check if the buttons has been pressed
-    if (buttons.isPressed(0)) {
-      Serial.println("Button A pressed!");
-    } else if (buttons.isPressed(1)) {
-      Serial.println("Button B pressed!");
-    } else if (buttons.isPressed(2)) {
-      Serial.println("Button C pressed!");
-    }
-  }
-}
+ ModulinoButtons buttons;
+ 
+ bool button_a = false;
+ bool button_b = false;
+ bool button_c = false;
+ 
+ void setup() {
+   Serial.begin(9600);
+   Modulino.begin();
+   buttons.begin();
+   //function to control the LEDs on top of each button
+   buttons.setLeds(button_a, button_b, button_c);
+ }
+ 
+ void loop() {
+   //request new data from the Modulino buttons
+   if (buttons.update()) {
+     //Check if the buttons has been pressed
+     if (buttons.isPressed(0)) {
+       Serial.println("Button A pressed!");
+       button_a = !button_a;
+     } else if (buttons.isPressed(1)) {
+       Serial.println("Button B pressed!");
+       button_b = !button_b;
+     } else if (buttons.isPressed(2)) {
+       Serial.println("Button C pressed!");
+       button_c = !button_c;
+     }
+     
+     buttons.setLeds(button_a, button_b, button_c);
+   }
+ }
